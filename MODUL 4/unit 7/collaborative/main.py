eed6d4e3e50371faa7de3165895d174032d72d87
rojbincelik <rojbinkarakoccelik2026@gmail.com>
2026-09-13 14:58:31 +0300

    unit 7 collaborative

---
+++ MODUL 4/unit 7/collaborative/main.py
@@ -0,0 +1,11 @@
+# main.py 
+print("Welcom to the Student System")  # note the typo 
+def search_student(name):
+    print(f"Searching for {name}...") 
+
+def main(): 
+    pass 
+
+
+if __name__ == "__main__": 
+    main() 
